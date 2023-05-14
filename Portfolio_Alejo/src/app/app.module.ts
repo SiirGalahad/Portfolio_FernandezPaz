45278907f3af49de93558c4ca920d2d298905d8f
SiirGalahad <aleepaz03@gmail.com>
2023-05-14 09:57:15 -0300

    Añadida educaion y exp

--- Portfolio_Alejo/src/app/app.module.ts
+++ Portfolio_Alejo/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienceComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
