49b646fcef0b03d1e6e308f67334e1358fa45fc6
SiirGalahad <aleepaz03@gmail.com>
2023-05-14 15:01:55 -0300

    update

---
+++ Portfolio_Alejo/BackEnd/src/main/java/com/portfolio/FPA/FpaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.FPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpaApplication.class, args);
+	}
+
+}
---
+++ Portfolio_Alejo/BackEnd/src/test/java/com/portfolio/FPA/FpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.FPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
